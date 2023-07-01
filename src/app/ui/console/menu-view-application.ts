56f48a962e01e62bb3e6cde8bcc258f6f45806bd
ladimiroBC <ladimiro1983@hotmail.com>
2023-06-30 23:22:25 -0500

    refactorizando

--- src/app/ui/console/menu-view-application.ts
+++ src/app/ui/console/menu-view-application.ts
@@ -1,10 +1,11 @@
+import { Menu } from "../../common/constants/view-menu-text";
 import { IMenuView } from "../../domain/interface/menu-view-interface";
 
 export class MenuView implements IMenuView{
   enumValues: string[];
 
   imprimirMenu(): void {
-    this.enumValues = Object.values(MenuView) as string[];
+    this.enumValues = Object.values(Menu) as string[];
     this.enumValues.forEach((value) => {
       console.log(value);
     });
