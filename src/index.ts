eef4018896100e5718e673a5efaa92981f9d615a
ladimiroING <ladimiro1010@yahoo.com>
2023-07-20 12:19:26 -0500

    Iniciando desde webpack

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,5 @@
-import { Application } from "./app/app";
-import { MenuMachineService } from "./app/application/services/menu.machine.service";
-import { MenuView } from "./app/common/utils/menu.view.application";
-import { MessagesApp } from "./app/common/utils/messages.application";
-import { ViewsApplication } from "./app/ui/console/components/views.application";
 
-const app: Application = new Application(new ViewsApplication(
-  new MenuMachineService, new MessagesApp, new MenuView));
+console.log("Hola desde webpack :)....");
 
-let flag = true;
-
-while (flag) {
-
-  app.myStartConsole();
-
-  if (app.myStartConsole() == false) {
-    flag = false;
-  }
-}
 
 
